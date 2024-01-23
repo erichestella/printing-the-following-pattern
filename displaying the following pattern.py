3b45e232de6237f190d28a8faa208941be37641b
erichestella <erichestella05@gmail.com>
2024-01-24 00:26:34 +0800

    it illustrate the rows you just type

--- displaying the following pattern.py
+++ displaying the following pattern.py
@@ -4,3 +4,9 @@ print('\nPRINT THE FOLLOWING PATTERN.\n')
 #adding some string to the variable 
 numeral = int(input('Type the number you want to print: '))
 
+#function to display the rows you want to show
+for vertical_row in range(-1, numeral+1):
+    for horizontal_column in range(1, vertical_row + 1):
+           print(vertical_row, end= '   ')
+    print()
+
